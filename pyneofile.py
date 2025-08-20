a0394da99a4f8a70063083003a7b593eee19445e
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2025-08-20 15:48:36 -0500

    Small update

--- pyneofile.py
+++ pyneofile.py
@@ -65,7 +65,7 @@ __version_date_info__ = (2025, 8, 18, "RC 1", 1)
 __version_date__ = str(__version_date_info__[0]) + "." + str(
     __version_date_info__[1]).zfill(2) + "." + str(__version_date_info__[2]).zfill(2)
 __revision__ = __version_info__[3]
-__revision_id__ = "$Id: e8f430ca2dd331b705df7e8107d81e61ea796166 $"
+__revision_id__ = "$Id$"
 if(__version_info__[4] is not None):
     __version_date_plusrc__ = __version_date__ + \
         "-" + str(__version_date_info__[4])
