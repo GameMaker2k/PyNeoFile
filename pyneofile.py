766736aee5729a737667cd26224660624f5da657
Kazuhana Neko-chan <kazuki.suzuki.fox@gmail.com>
2025-11-03 20:32:28 -0600

    Ver up next

--- pyneofile.py
+++ pyneofile.py
@@ -60,7 +60,7 @@ __use_ini_name__ = "neofile.ini"
 __program_name__ = "PyNeoFile"
 __project__ = __program_name__
 __project_url__ = "https://github.com/GameMaker2k/PyNeoFile"
-__version_info__ = (0, 24, 4, "RC 1", 1)
+__version_info__ = (0, 24, 6, "RC 1", 1)
 __version_date_info__ = (2025, 11, 3, "RC 1", 1)
 __version_date__ = str(__version_date_info__[0]) + "." + str(
     __version_date_info__[1]).zfill(2) + "." + str(__version_date_info__[2]).zfill(2)
