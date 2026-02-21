af499485d177e01bdc9f6653f954b1af8d087b8f
Tsukiumi Neko-kun <kazuki.suzuki.wa@gmail.com>
2026-02-20 20:16:00 -0600

    Small update

--- pyneofile/pyfile.py
+++ pyneofile/pyfile.py
@@ -7989,7 +7989,7 @@ def NeoFileValidate(infile, fmttype="auto", filestart=0, formatspecs=__file_form
             infile = RemoveWindowsPath(infile)
         listarrayfileslist = ArchiveFileToArray(
             infile, fmttype, filestart, 0, 0,
-            False, False, True, True, formatspecs, saltkey, seektoend, returnfp
+            False, True, False, True, formatspecs, saltkey, seektoend, returnfp
         )
     for listarrayfiles in listarrayfileslist:
         fp = listarrayfiles['fp']
