64a3c594061d306ad6aa6bf4dc7884727b393950
Tsukiumi Neko-kun <kazuki.suzuki.wa@gmail.com>
2026-02-23 00:40:13 -0600

    Small update

--- neofile.py
+++ neofile.py
@@ -464,7 +464,7 @@ def main(argv: Optional[list[str]] = None) -> int:
                     getargs.outsecretkey,
                     False,
                     False,
-                    False,
+                    True,
                 )
             else:
                 tmpout = pyneofile.PackNeoFileFromInFile(
@@ -481,7 +481,7 @@ def main(argv: Optional[list[str]] = None) -> int:
                     fnamedict,
                     getargs.outsecretkey,
                     False,
-                    False,
+                    True,
                 )
 
             input_file = tempout
