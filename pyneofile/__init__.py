a553c44ca1f640dcb6a07b20f42fe365179c63fe
Tsukiumi Neko-kun <kazuki.suzuki.wa@gmail.com>
2026-02-23 16:06:24 -0600

    Small update

--- pyneofile/__init__.py
+++ pyneofile/__init__.py
@@ -14,28 +14,28 @@
     Copyright 2018-2026 Game Maker 2k - http://intdb.sourceforge.net/
     Copyright 2018-2026 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
 
-    $FileInfo: pyfile.py - Last Update: 2/8/2026 Ver. 0.28.8 RC 1 - Author: cooldude2k $
+    $FileInfo: __init__.py - Last Update: 2/8/2026 Ver. 0.28.8 RC 1 - Author: cooldude2k $
 '''
 
-from .pyfile import *
-from . import pyfile
+from .pyneofile import *
+from . import pyneofile
 
-__project__ = pyfile.__project__
-__program_name__ = pyfile.__program_name__
-__file_format_name__ = pyfile.__file_format_name__
-__file_format_magic__ = pyfile.__file_format_magic__
-__file_format_len__ = pyfile.__file_format_len__
-__file_format_hex__ = pyfile.__file_format_hex__
-__file_format_delimiter__ = pyfile.__file_format_delimiter__
-__file_format_dict__ = pyfile.__file_format_dict__
-__file_format_default__ = pyfile.__file_format_default__
-__file_format_multi_dict__ = pyfile.__file_format_multi_dict__
-__use_new_style__ = pyfile.__use_new_style__
-__use_advanced_list__ = pyfile.__use_advanced_list__
-__use_alt_inode__ = pyfile.__use_alt_inode__
-__project_url__ = pyfile.__project_url__
-__version_info__ = pyfile.__version_info__
-__version_date_info__ = pyfile.__version_date_info__
-__version_date__ = pyfile.__version_date__
-__version_date_plusrc__ = pyfile.__version_date_plusrc__
-__version__ = pyfile.__version__
+__project__ = pyneofile.__project__
+__program_name__ = pyneofile.__program_name__
+__file_format_name__ = pyneofile.__file_format_name__
+__file_format_magic__ = pyneofile.__file_format_magic__
+__file_format_len__ = pyneofile.__file_format_len__
+__file_format_hex__ = pyneofile.__file_format_hex__
+__file_format_delimiter__ = pyneofile.__file_format_delimiter__
+__file_format_dict__ = pyneofile.__file_format_dict__
+__file_format_default__ = pyneofile.__file_format_default__
+__file_format_multi_dict__ = pyneofile.__file_format_multi_dict__
+__use_new_style__ = pyneofile.__use_new_style__
+__use_advanced_list__ = pyneofile.__use_advanced_list__
+__use_alt_inode__ = pyneofile.__use_alt_inode__
+__project_url__ = pyneofile.__project_url__
+__version_info__ = pyneofile.__version_info__
+__version_date_info__ = pyneofile.__version_date_info__
+__version_date__ = pyneofile.__version_date__
+__version_date_plusrc__ = pyneofile.__version_date_plusrc__
+__version__ = pyneofile.__version__
