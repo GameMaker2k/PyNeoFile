3305e42a37d51db9462690bf9e2f5a2e544f3d36
Tsukiumi Neko-kun <kazuki.suzuki.wa@gmail.com>
2026-03-04 19:30:33 -0600

    Small update

--- pyneofile/pyneofile.py
+++ pyneofile/pyneofile.py
@@ -9715,7 +9715,7 @@ def UnPackNeoFile(infile, outdir=None, followlink=False, filestart=0, seekstart=
     else:
         if(infile != "-" and not hasattr(infile, "read") and not hasattr(infile, "write") and not isinstance(infile, bytes)):
             infile = RemoveWindowsPath(infile)
-        listarrayfilespre = NeoFileToArray(infile, "auto", filestart, seekstart, seekend, False, True, True, skipchecksum, formatspecs, saltkey, seektoend, returnfp)
+        listarrayfilespre = ArchiveFileToArray(infile, "auto", filestart, seekstart, seekend, False, True, True, skipchecksum, formatspecs, saltkey, seektoend, returnfp)
     if(not listarrayfilespre):
         return False
     if(not isinstance(listarrayfilespre, list)):
@@ -9785,7 +9785,7 @@ def UnPackNeoFile(infile, outdir=None, followlink=False, filestart=0, seekstart=
                 if(preservetime):
                     os.utime(PrependPath(outdir, listarrayfiles['ffilelist'][lcfi]['fname']), (
                         listarrayfiles['ffilelist'][lcfi]['fatime'], listarrayfiles['ffilelist'][lcfi]['fmtime']))
-            if(listarrayfiles['ffilelist'][lcfi]['ftype'] == 1):
+            elif(listarrayfiles['ffilelist'][lcfi]['ftype'] == 1):
                 if(followlink):
                     getflinkpath = listarrayfiles['ffilelist'][lcfi]['flinkname']
                     flinkid = prelistarrayfiles['filetoid'][getflinkpath]
@@ -9860,7 +9860,7 @@ def UnPackNeoFile(infile, outdir=None, followlink=False, filestart=0, seekstart=
                 else:
                     os.link(listarrayfiles['ffilelist'][lcfi]['flinkname'], PrependPath(
                         outdir, listarrayfiles['ffilelist'][lcfi]['fname']))
-            if(listarrayfiles['ffilelist'][lcfi]['ftype'] == 2):
+            elif(listarrayfiles['ffilelist'][lcfi]['ftype'] == 2):
                 if(followlink):
                     getflinkpath = listarrayfiles['ffilelist'][lcfi]['flinkname']
                     flinkid = prelistarrayfiles['filetoid'][getflinkpath]
@@ -9935,7 +9935,7 @@ def UnPackNeoFile(infile, outdir=None, followlink=False, filestart=0, seekstart=
                 else:
                     os.symlink(listarrayfiles['ffilelist'][lcfi]['flinkname'], PrependPath(
                         outdir, listarrayfiles['ffilelist'][lcfi]['fname']))
-            if(listarrayfiles['ffilelist'][lcfi]['ftype'] == 5):
+            elif(listarrayfiles['ffilelist'][lcfi]['ftype'] == 5):
                 if(preservepermissions):
                     os.mkdir(PrependPath(
                         outdir, listarrayfiles['ffilelist'][lcfi]['fname']), listarrayfiles['ffilelist'][lcfi]['fchmode'])
@@ -9951,12 +9951,16 @@ def UnPackNeoFile(infile, outdir=None, followlink=False, filestart=0, seekstart=
                 if(preservetime):
                     os.utime(PrependPath(outdir, listarrayfiles['ffilelist'][lcfi]['fname']), (
                         listarrayfiles['ffilelist'][lcfi]['fatime'], listarrayfiles['ffilelist'][lcfi]['fmtime']))
-            if(listarrayfiles['ffilelist'][lcfi]['ftype'] == 6 and hasattr(os, "mkfifo")):
+            elif(listarrayfiles['ffilelist'][lcfi]['ftype'] == 6 and hasattr(os, "mkfifo")):
                 os.mkfifo(PrependPath(
                     outdir, listarrayfiles['ffilelist'][lcfi]['fname']), listarrayfiles['ffilelist'][lcfi]['fchmode'])
-            if(returnfp):
-                fplist.append(listarrayfiles['ffilelist'][lcfi]['fp'])
+            elif((listarrayfiles['ffilelist'][lcfi]['ftype'] == 3 or listarrayfiles['ffilelist'][lcfi]['ftype'] == 4) and hasattr(os, "makedev") and hasattr(os, "mknod")):
+                outdev = os.makedev(listarrayfiles['ffilelist'][lcfi]['frdev_major'], listarrayfiles['ffilelist'][lcfi]['frdev_minor'])
+                os.mknod(PrependPath(
+                    outdir, listarrayfiles['ffilelist'][lcfi]['fname']), listarrayfiles['ffilelist'][lcfi]['fchmode'], outdev)
             lcfi = lcfi + 1
+        if(returnfp):
+            fplist.append(listarrayfiles['fp'])
     if(returnfp):
         return fplist
     else:
